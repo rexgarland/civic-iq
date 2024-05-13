3b179fb666a5024872a0da1211475b27b1735491
Rex Garland <rex@rexgarland.dev>
2024-05-12 22:05:35 -0700

    Gulp html

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,6 @@
 import gulp from "gulp";
+import htmltidy from "gulp-htmltidy";
 
-export default function (cb) {
-  console.log("Gulp running");
-  cb();
+export default function html() {
+  return gulp.src("public/index.html").pipe(htmltidy()).pipe(gulp.dest("dist"));
 }
