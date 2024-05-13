8fa01885e30bd702df94c0111024525fb5e11f86
Rex Garland <rex@rexgarland.dev>
2024-05-12 21:02:49 -0700

    Ignore more artifacts

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  presets: [
+    [
+      "@babel/preset-env",
+      { targets: { browsers: "> 0.25%, not dead", node: "current" } },
+    ],
+  ],
 };
